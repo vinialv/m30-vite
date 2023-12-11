29fef9c8b42b3f8ba0759b14d73c26cfba947c86
Vinicius Oliveira Alves <vini.alv15@hotmail.com>
2023-12-11 19:56:54 -0300

    Ajustes finais na animação

--- src/components/Arquitetura30SVG/styles.ts
+++ src/components/Arquitetura30SVG/styles.ts
@@ -1,19 +1,19 @@
-import styled, { keyframes } from 'styled-components'
+import styled, { keyframes } from "styled-components";
 
-const elementHeight = '100px'
+const elementHeight = "100px";
 
 const show30arquitetura = keyframes`
   to {
     visibility: visible;
   }
-`
+`;
 
 export const Arquitetura30 = styled.svg`
   right: 21.8%;
-  top: 39.25%;
+  top: 39.2%;
   position: absolute;
   visibility: hidden;
   height: calc(0.819 * ${elementHeight});
   animation: ${show30arquitetura} 0s linear 3.25s forwards;
   -webkit-animation: ${show30arquitetura} 0s linear 3.25s forwards;
-`
+`;
--- src/components/LogoAnimation/styles.ts
+++ src/components/LogoAnimation/styles.ts
@@ -1,16 +1,18 @@
-import styled, { keyframes } from 'styled-components'
+import styled, { keyframes } from "styled-components";
 
-const elementHeight = '100px'
+const elementHeight = "100px";
 
 const showContainer = keyframes`
   to {
     visibility: visible;
+    opacity: 1;
   }
-`
+`;
 
 export const Container = styled.div`
   display: grid;
-  margin-bottom: 17.5px;
+  margin-bottom: 16.5px;
+  opacity: 0;
   visibility: hidden;
   position: relative;
   align-items: center;
@@ -18,6 +20,6 @@ export const Container = styled.div`
   width: calc(3 * ${elementHeight});
   height: calc(3 * ${elementHeight});
   grid-template-columns: repeat(3, 1fr);
-  animation: ${showContainer} 0s linear 1.25s forwards;
-  -webkit-animation: ${showContainer} 0s linear 1.25s forwards;
-`
+  animation: ${showContainer} 0.35s linear 1.25s forwards;
+  -webkit-animation: ${showContainer} 0.35s linear 1.25s forwards;
+`;
