6adc83546ce83cad1ee2fa87898d160d35374b01
Vinicius Oliveira Alves <vini.alv15@hotmail.com>
2023-12-11 20:17:54 -0300

    Inserindo lib da vercel no projeto

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
+  <>
     <Analytics />
-    <SpeedInsights />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+      <SpeedInsights />
+    </React.StrictMode>
+  </>
 );
